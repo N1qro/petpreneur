abbd80697b999197101f488037f677ad5ad1aca9
Andrew <andrey777-novikov777@yandex.ru>
2023-12-04 18:58:13 +0300

    Initial project setup for development
    + README
    + All documentation apps setup
    + Requirements setup
    + CI/CD setup
    + .gitignore setup
    + flake8 configuration

---
+++ petpreneur/matcher/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class MatcherConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "matcher"
+
+
+__all__ = []
---
+++ petpreneur/petpreneur/settings.py
@@ -0,0 +1,98 @@
+import pathlib
+
+import environ
+
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+env = environ.Env(
+    DJANGO_DEBUG=(bool, True),
+    DJANGO_SECRET_KEY=(str, "DJANGO_DEFAULT_KEY"),
+    DJANGO_ALLOWED_HOSTS=(list, ["127.0.0.1", "localhost"]),
+)
+env.read_env(BASE_DIR.parent / ".env")
+
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+DEBUG = env("DJANGO_DEBUG")
+ALLOWED_HOSTS = env("DJANGO_ALLOWED_HOSTS")
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "about.apps.AboutConfig",
+    "categories.apps.CategoriesConfig",
+    "core.apps.CoreConfig",
+    "feedback.apps.FeedbackConfig",
+    "home.apps.HomeConfig",
+    "jobs.apps.JobsConfig",
+    "matcher.apps.MatcherConfig",
+    "resume.apps.ResumeConfig",
+    "users.apps.UsersConfig",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "petpreneur.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "petpreneur.wsgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation."
+        "NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = "ru"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = "static/"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
