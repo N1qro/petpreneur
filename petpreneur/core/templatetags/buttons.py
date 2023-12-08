3e97677b978ab301326ec8a3fba80a0c6194b5d0
Andrew <andrey777-novikov777@yandex.ru>
2023-12-08 14:09:36 +0300

    Added landing page

---
+++ petpreneur/core/templatetags/buttons.py
@@ -0,0 +1,39 @@
+import typing
+
+import django.template
+
+
+register = django.template.Library()
+StateType = typing.Literal["primary", "hovered", "focused"]
+ButtonColor = typing.Literal["default", "black"]
+
+
+@register.inclusion_tag("includes/button.html")
+def button(
+    text: str, state: StateType = "primary", color: ButtonColor = "default",
+):
+    return {
+        "tag": "button",
+        "text": text,
+        "state": state,
+        "color": color,
+    }
+
+
+@register.inclusion_tag("includes/button.html")
+def link_button(
+    text: str,
+    href: str = "#",
+    state: StateType = "primary",
+    color: ButtonColor = "default",
+):
+    return {
+        "tag": "a",
+        "text": text,
+        "state": state,
+        "color": color,
+        "href": href,
+    }
+
+
+__all__ = []
