6c94bb4f5f48a56e0f6b4053dc93a26a091064aa
Andrew <andrey777-novikov777@yandex.ru>
2023-12-09 23:28:45 +0300

    Improved navbar, landing; Started work on profile

--- petpreneur/home/urls.py
+++ petpreneur/home/urls.py
@@ -2,7 +2,8 @@ import django.urls
 
 import home.views
 
+app_name = "home"
 
 urlpatterns = [
-    django.urls.path("", home.views.home),
+    django.urls.path("", home.views.home, name="home"),
 ]
