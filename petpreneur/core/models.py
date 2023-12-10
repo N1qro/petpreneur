9700586a467f161e7da14c1a1b6e13cf0e2fc2e4
regrit <rebogaba@gmail.com>
2023-12-10 20:02:53 +0700

    add resume and job models | updated core

--- petpreneur/core/models.py
+++ petpreneur/core/models.py
@@ -1,3 +1,4 @@
+import django.conf
 import django.db.models
 
 
@@ -14,6 +15,44 @@ class AbstractNameSlugModel(django.db.models.Model):
         max_length=150,
         unique=True,
         help_text="Максимальная длина - 150 символов",
+    )
+
+    class Meta:
+        abstract = True
+
+
+class AbstractTextCreatedAtModel(django.db.models.Model):
+    text = django.db.models.TextField(
+        verbose_name="текст",
+        help_text="Введите текст",
+    )
+    created_at = django.db.models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="дата и время создания",
+        help_text="Это поле создается автоматически",
+    )
+
+    class Meta:
+        abstract = True
+
+
+class AbstractIsActiveUserIdCategoryIdSubcategoryId(django.db.models.Model):
+    is_active = django.db.models.BooleanField(
+        default=True,
+        verbose_name="активно",
+        help_text="Поставьте галочку, если хотите сделать активным",
+    )
+    user = django.db.models.ForeignKey(
+        django.conf.settings.AUTH_USER_MODEL,
+        on_delete=django.db.models.CASCADE,
+    )
+    category = django.db.models.ForeignKey(
+        to="categories.Category",
+        on_delete=django.db.models.CASCADE,
+    )
+    subcategory = django.db.models.ForeignKey(
+        to="categories.Subcategory",
+        on_delete=django.db.models.CASCADE,
     )
 
     class Meta:
